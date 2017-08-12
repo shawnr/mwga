9e549c44f51953358e698066bf774d19f49e2b53
Shawn Rider <shawn@shawnrider.com>
2017-08-12 15:07:39 -0700

    Initial commit.
    Change-Id: I1d7403c3a512bcf6dc9dfe705772a804caf73f6e

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mwgaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mwgaApp
+ */
+angular.module('mwgaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mwgaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mwgaApp
+ */
+angular.module('mwgaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
