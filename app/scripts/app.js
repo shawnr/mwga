764086cae471328191137488fe467d3179d0d3a2
Shawn Rider <shawn@shawnrider.com>
2017-08-12 17:49:56 -0700

    Added caching to the weather API calls.
    Change-Id: Ie3416c0ea742927127612b02576bc27cdef6c125

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ngStorage'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,14 +8,47 @@
  * Controller of the mwgaApp
  */
 angular.module('mwgaApp')
-  .controller('MainCtrl', function ($scope, current, forecast, citycheck) {
+  .controller('MainCtrl', function ($scope, current, forecast, citycheck, $localStorage) {
     $scope.showCurrent = false;
     $scope.showForecast = false;
     $scope.showInvalidCity = false;
     $scope.dataLoading = false;
     $scope.showSearch = true;
-    $scope.current = current.query();
-    $scope.forecast = forecast.query();
+
+    var now = Date.now();
+    if ($localStorage.cacheTimestamp){
+        var cacheTime = new Date($localStorage.cacheTimestamp);
+        var nowTime = new Date(now);
+        var timeDelta = Math.round((nowTime - cacheTime) / (60 * 1000));
+        if (timeDelta > 10){
+            console.log('Cache is expired. Refreshing.');
+            $scope.current = current.query();
+            $scope.current.$promise.then(function(data){
+                $localStorage.currentCache = data;
+            });
+            $scope.forecast = forecast.query();
+            $scope.forecast.$promise.then(function(data){
+                $localStorage.forecastCache = data;
+            })
+            $localStorage.cacheTimestamp = now;
+        } else {
+            console.log('Cache is still fresh. Loading from local storage.');
+            $scope.current = $localStorage.currentCache;
+            $scope.forecast = $localStorage.forecastCache;
+        }
+
+    } else {
+        console.log('Cache does not exist. Creating.');
+        $scope.current = current.query();
+        $scope.current.$promise.then(function(data){
+            $localStorage.currentCache = data;
+        });
+        $scope.forecast = forecast.query();
+        $scope.forecast.$promise.then(function(data){
+            $localStorage.forecastCache = data;
+        })
+        $localStorage.cacheTimestamp = now;
+    }
 
     $scope.openCurrent = function(){
         $scope.dataLoading = true;
