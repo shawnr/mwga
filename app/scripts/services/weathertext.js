8f3663d4d1fa7f33496bb89cc81fe99eadef0287
Shawn Rider <shawn@shawnrider.com>
2017-08-15 11:24:46 -0700

    Fixed mobile styles, added more text.
    Change-Id: I29ad5c97b7fbcd003ae0a1e67905f0c93070cb9f

--- app/scripts/services/weathertext.js
+++ app/scripts/services/weathertext.js
@@ -15,52 +15,69 @@ angular.module('mwgaApp')
         "It's going to rain very hard. It will be a little bit scary. A little bit noisy. But we will do OK.",
         "If Obama had done his job, we wouldn't have this thunderstorm to worry about. But here we are.",
         "We will use the full force of the American military to fight this thunderstorm.",
-        "I will present a plan in 30 days for eliminating this thunderstorm once and for all."
+        "I will present a plan in 30 days for eliminating this thunderstorm once and for all.",
+        "There was a thunderstorm on Inauguration Day until God parted the clouds to let me speak. I was beautiful. And my words were the best.",
+        "I have stopped more thunderstorms during my administration than any other US President. Only Mussolini ranked higher than me!"
       ],
       // lightning 221, 212, 211, 210
       lightning: [
         "There will be lightning. Which is electricity. I know all about it because my grandfather was an electrician back in World War II and he told me everything.",
-        "Electricity is very powerful stuff. It's hot. And tingly. I know, because I have a very good brain."
+        "Electricity is very powerful stuff. It's hot. And tingly. I know, because I have a very good brain.",
+        "The lightning strikes like we will strike North Korea (or Venezuala, or Iran, or Syria, etc.): With Fire and Fury!",
+        "Fire and fury!"
       ],
       // sprinkle 500, 321, 301, 300
       sprinkle: [
         "There will be small showers. Sprinkles. It will sprinkle, but only a small amount.",
         "The rain will be so hard today because the Democrats continue to refuse to work with me.",
-        "Unlike my own strong hands, these sprinkles will be very small rain showers."
+        "Unlike my own strong hands, these sprinkles will be very small rain showers.",
+        "On Inauguration Day a light sprinkle turned into a raging storm until I began to speak."
       ],
       // rain 504, 503, 502, 501, 314, 312, 311, 302
       rain: [
         "It's going to be wet. Rainy. The rain will fall. Be careful out there.",
         "It's not going to rain. The main stream media says it will, but it won't. Trust me. I know.",
         "If it DOES rain today, it's because the Democrats failed to work with us on healthcare.",
-        "Maybe if Obama had been more like Putin then we wouldn't have all this rain now."
+        "Maybe if Obama had been more like Putin then we wouldn't have all this rain now.",
+        "It's definitely not raining today, no matter what the main stream media wants us to believe."
       ],
       // rain-mix 620, 616, 615, 612, 611, 511, 310
       rainMix: [
         "It will be rain, but not just rain. Rain and something else. Rain plus something extraordinary. Because it is America, and that's who we do.",
         "It might be rainy. It might not. Depends on which side you believe. There are many sides to this issues. We will figure it out.",
-        "This great military of ours is so great. This rain will not keep them from winning!"
+        "This great military of ours is so great. This rain will not keep them from winning!",
+        "The rain is really terrible. A new low for Mother Nature. She's just a nasty woman. She rains out of her sky, she rains out of her wherever.",
+        "Inauguration Day was a mix of rain and lies from the main stream media about the largest crowd ever assembled."
       ],
       // showers 701, 522, 521, 520, 313
       showers: [
         "There will be showers. It will shower us with wealth. Showers of gold. Golden showers. For all of us. So wonderful.",
         "The main stream media is continuing to promote the LIE that there will be showers today. Do not believe it. There will be no showers.",
-        "Unlike my hands, which are beautiful and larger than normal, showers are smaller than rain."
+        "Unlike my hands, which are beautiful and larger than normal, showers are smaller than rain.",
+        "Showers are coming, they say. The showers will get us all damp, they say. Well, who do they think they are? We won't have showers. We will have sunshine!",
+        "These showers will be stronger than Hillary Clinton's tears after she realized I won the popular election."
       ],
       // storm-showers 901, 521
       stormShowers: [
         "There will be showers, but there will also be storms. We will enjoy the showers. But we will not enjoy the loud storms.",
-        "There will be thunderstorms. And there will be showers. There will be some of both. This is an issue with many sides."
+        "There will be thunderstorms. And there will be showers. There will be some of both. This is an issue with many sides.",
+        "With the rain, and the wind, and the thunder and lightning. It's like Vietnam out there. Except worse.",
+        "These showers will rain down like the tears from my many, many enemies. Happy Mother's Day."
       ],
       // snow 903, 622, 621, 601, 600
       snow: [
         "Snow will fall and it will be white and pure. So beautiful.",
-        "As my father taught me: Snow is cold."
+        "As my father taught me: Snow is cold. So we don't want that. Let's take care of it.",
+        "I love snow, so beautiful and white. Some people don't. But some people know what I mean. I love whites.",
+        "The snow is just gorgeous. Big, big drifts remind me of, well, you know. The things we like best about women. Right?",
+        "It's cold. It's frigid. It's not a good day for us. This is what happens when we don't put America First."
       ],
       // sleet 602
       sleet: [
         "Nobody likes sleet. But we must live with it because this is the system we have. For now.",
-        "Is it ice? Or is it rain? Is it very wet snow? There are many sides to this issue. It's a bad time for America."
+        "Is it ice? Or is it rain? Is it very wet snow? There are many sides to this issue. It's a bad time for America.",
+        "It's wet. It's cold. It's icy. No more questions.",
+        "Today will be beautiful beyond belief."
       ],
       // smoke 711
       smoke: [
@@ -71,46 +88,60 @@ angular.module('mwgaApp')
       // day-haze 721
       dayHaze: [
         "Today will be hazy. Nobody will really know what's out there. But do any of us really know? What is out there? Who knows? Maybe someday we will. But not now. Let's figure that out.",
-        "The main stream media wants you to believe it will be hazy today. But that is just what it looks like when we put America first!"
+        "The main stream media wants you to believe it will be hazy today. But that is just what it looks like when we put America first!",
+        "Covfefe."
       ],
       // dust 762, 761, 731
       dust: [
         "The dust will be terrible today. Just horrible. Bad hombre dust that will make you have to use that nose thing.",
-        "Why worry about a little dust? I'm a very intelligent guy, and I'm not bothered by dust. We won't let them keep telling us to be worried about dust!"
+        "Why worry about a little dust? I'm a very intelligent guy, and I'm not bothered by dust. We won't let them keep telling us to be worried about dust!",
+        "Covfefe."
       ],
       // fog 741
       fog: [
         "It's foggy out there. Can't see a thing. Terrible for golf.",
-        "Some people like fog. And I respect fog. But I like sunshine better."
+        "Some people like fog. And I respect fog. But I like sunshine better.",
+        "It's so foggy that the main stream media couldn't even see the full size of the audience at my rally. Biggest audience yet."
       ],
       // cloudy-gusts 801, 802, 803, 771
       cloudyGusts: [
         "The clouds are out and the wind is blowing hard. Might be a good day for a hat. I sell those. Best hats ever made. Made in America. From REAL cotton.",
-        "We have problems with sunshine and cloudiness on both sides. This is bad for our country."
+        "We have problems with sunshine and cloudiness on both sides. This is bad for our country.",
+        "We have the clouds. We have the wind. What else? Who really knows?"
       ],
       // tornado 900, 781
       tornado: [
         "A tornado may happen. Or it may be happening. I'm not sure. I don't pay attention to that sort of thing. Good luck.",
         "The main stream media will tell you to take shelter from this tornado, but that's FAKE NEWS. It will be windy, but we will make it.",
-        "If this tornado keeps threatening the area, it will feel the full fire and fury of the US military."
+        "If this tornado keeps threatening the area, it will feel the full fire and fury of the US military.",
+        "Covfefe.",
+        "I have stopped more tornadoes during my administration than any other US President. Only Putin ranks higher than me!"
       ],
       // day-sunny 800
       daySunny: [
         "It's a lovely, sunny day. I'd go golfing, but I have too much work to do. Well, maybe in the afternoon. We'll see.",
         "It's a great day for a round of golf at one of my many lovely golf courses. We have the best greens. They are the best.",
         "Let me tell you, folks, you're going to get sick and tired of all this sunshine.",
-        "This is what it looks like when we put America First!"
+        "This is what it looks like when we put America First!",
+        "It's sunny. Let's take the boat out. I have a friend who bought a boat after he sold his company and made a lot of money. He did things on that boat, well, I won't repeat. But you can imagine. You're a person of the world.",
+        "Good sun. Very warm. Good genes.",
+        "This is the kind of day when we can make a lot of money. I mean a lot of money. Like, lots. You won't believe it.",
+        "It's warm. Too warm? Or not warm enough? There are many sides to this issue."
       ],
       // cloudy 804
       cloudy: [
         "The clouds are out, but it's OK: There is no rain in the forecast. We will be OK, and we will make it.",
         "It could be cloudy. It could be sunny. It could be something else. Who knows?",
-        "Some people like clouds. Some people do not. At least we can agree: we all hate rain."
+        "Some people like clouds. Some people do not. At least we can agree: we all hate rain.",
+        "Clouds are like Democrats coming to ruin an otherwise great healthcare bill. The greatest. Sad.",
+        "The clouds are bad, people. They bring rain. They bring bad air. They are bad hombres."
       ],
       // hurricane 902
       hurricane: [
         "A hurricane might be happening. Or maybe not. I would tell you to check with the weather guys, but I think I cut that budget. Good luck!",
-        "Hurricanes are a story made up by the Chinese to get back at our great country for our success. Hillary would tell you to take cover from it, but I know those things and they're not that bad. Very big. Very windy. But not that bad."
+        "Hurricanes are a story made up by the Chinese to get back at our great country for our success. Hillary would tell you to take cover from it, but I know those things and they're not that bad. Very big. Very windy. But not that bad.",
+        "Sad!",
+        "I have stopped more hurricanes during my administration than any other US President. Only Pol Pot ranked higher than me!"
       ],
       // hot 904
       hot: [
@@ -121,12 +152,15 @@ angular.module('mwgaApp')
       // windy 957, 905
       windy: [
         "Wind is blowing around everywhere. Be careful out there. Might want to buy a MAGA hat. #MAGA",
-        "Sure, it's windy. But look at us: Are we so innocent?"
+        "Sure, it's windy. But look at us: Are we so innocent?",
+        "Not a good day for golfing."
       ],
       // hail 906
       hail: [
         "There is hail. It might be the size of a pea. It might be more like a golf ball. Nobody knows. But we're going to find out.",
-        "Once a hail stone broke the window of my limo. I had to use a different limo for, like, two weeks. It was my personal Vietnam."
+        "Once a hail stone broke the window of my limo. I had to use a different limo for, like, two weeks. It was my personal Vietnam.",
+        "A terrible day for golf. And America.",
+        "I have stopped more hail during my administration than any other US President. Only Mussolini ranked higher than me!"
       ]
     }
 
